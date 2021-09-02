fefaf3bb079daf1c74601bf2fa86a9bb8906ce1c
Khoi HOANG <khoi.hoang@exwzd.com>
2021-09-03 00:06:56 +0900

    Added Spring Boot files

---
+++ src/test/java/com/vietcatholicjp/isidore/IsidoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vietcatholicjp.isidore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsidoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
